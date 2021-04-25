7a6cba632d6228f4b9ac864ef6b346c93d5178c6
nicole chavarria <71100444+nicolenc614@users.noreply.github.com>
2021-04-24 21:17:04 -0400

    Update session.php

--- session.php
+++ session.php
@@ -1,17 +1,16 @@
 <?php
-  //php file that deals with the connection of the database
   $mysqli=mysqli_connect( "localhost","root","","hospitall");
-  if($mysqli-> connect_errno){//checks if there is an error
+  if($mysqli-> connect_errno){
     printf("ERROR");
   }
-  session_start();//starts the session for the user
-  $employeecheck=$_SESSION['user'];//assigns the user to a variable
-  $sess ="select employee_ID from employee where employee_ID='$employeecheck'";//checks if the user is in the database
+  session_start();
+  $employeecheck=$_SESSION['user'];
+  $sess ="select employee_ID from employee where employee_ID='$employeecheck'";
   $result=$mysqli->query($sess);
   $row=$result->fetch_assoc();
-  $login_session = $row['employee_ID'];//assigns the correct user to a variable
-  if(!isset($login_session)){//if the user does not exist
-    mysqli_close($mysqli); // Closing Connection
-    header('Location: index.php'); // Redirecting To Home Page
+  $login_session = $row['employee_ID'];
+  if(!isset($login_session)){
+    mysqli_close($mysqli); 
+    header('Location: index.php'); 
   }
  ?>
